77d7c6892fc9871dba215d15669527866b06eba8
Shawn Ren <doggy-ren@hotmail.com>
2019-02-11 15:47:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(shout)
   puts love = "I LOVE YOU GRANDMA!"
   if shout == love
     return "I LOVE YOU TOO PUMPKIN!"
-  elseif shout == shout.upcase && shout != love
+  elsif shout == shout.upcase && shout != love
   return "NO, NOT SINCE 1938!"
 else
   return "HUH?! SPEAK UP, SONNY!"
